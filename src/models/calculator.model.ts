a536d9b1d52be02ef7c42fe1a23d8af08dc55dad
sathvikaiyengar <sathix7@gmail.com>
2024-09-17 10:57:42 -0400

    new push
    pushed

--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -5,21 +5,25 @@ import { OperatorKeys } from '../enums/operator-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
 
 export class CalculatorModel implements ICalculatorModel {
+  private _buffer: string = '';
 
   public pressNumericKey(key: NumericKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressOperatorKey(key: OperatorKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    // this._buffer += key;
+    // if the key is numeric, then operator, numeric
+    const sum = 2 + 3
+    // this._buffer.evaluate
   }
 
   public display(): string {
-    throw new Error('Method not implemented.');
+    return this._buffer;
   }
 
 }
